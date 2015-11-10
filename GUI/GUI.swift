275b1337da3e5f5635f9127f1cc7903841c3f4a5
Markus Chmelar <mexx@devsub.net>
2015-11-10 15:18:48 +0100

    documented

--- GUI/GUI.swift
+++ GUI/GUI.swift
@@ -10,10 +10,16 @@ import Foundation
 
 public class GUI {
     public static func image() -> UIImage? {
+        // 
+        //  Problem: This needs to load the image from the GUI bundle instead of `nil` (mainBundle)
+        //
         return R.image.icon256
     }
     
     public static func customView() -> UINib? {
+        //
+        //  Problem: This needs to load the image from the GUI bundle instead of `nil` (mainBundle)
+        //
         return R.nib.customView.instance
     }
 }
